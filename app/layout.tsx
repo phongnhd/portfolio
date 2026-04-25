8d35024c09bbb4b021d54fab108219a3e7cadf4f
Phongnhd <phongnhd2605@gmail.com>
2026-04-25 16:02:01 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter, Geist } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
-export const runtime = "edge"; 
+
 const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 const inter = Inter({
